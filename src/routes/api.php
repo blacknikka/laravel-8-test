76a6d832ec1c80f08ab968916deccc984b016679
Yasuhiro <yasuhiro1026a@hotmail.com>
2021-11-03 18:11:09 +0900

    update api route

--- src/routes/api.php
+++ src/routes/api.php
@@ -18,4 +18,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('/memo', [ App\Http\Controllers\MemoController::class, 'showMemos']);
+Route::get('/memos', [ App\Http\Controllers\MemoController::class, 'showMemos']);
