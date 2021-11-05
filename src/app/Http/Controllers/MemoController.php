cc4f3268d935ea16a67276522430e4741641c4d8
Yasuhiro <yasuhiro1026a@hotmail.com>
2021-11-06 08:24:10 +0900

    Add test for MemoController

--- src/app/Http/Controllers/MemoController.php
+++ src/app/Http/Controllers/MemoController.php
@@ -12,9 +12,9 @@ class MemoController extends Controller
      * Get all memo items
      *
      * @param Request $request
-     * @return App\Models\Memo[]
+     * @return Memo[]
      */
-    public function showMemos(Request $request) {
+    public function showMemos(Request $request): iterable {
         return Memo::all();
     }
 
@@ -23,21 +23,48 @@ class MemoController extends Controller
      *
      * @param Request $request
      * @param number $id
-     * @return App\Models\Memo
+     * @return Memo
      */
-    public function getMemosById(Request $request, $id) {
-        return Memo::find($id);
+    public function getMemosById(Request $request, $id): Memo {
+        return Memo::findOrFail($id);
+    }
+
+    /**
+     * Create Memo
+     * @param MemoRequest $request
+     * @return Memo
+     */
+    public function createMemos(MemoRequest $request): Memo {
+        return Memo::create([
+            "title" => $request->input("title"),
+            "body" => $request->input("body"),
+            "status" => $request->input("status"),
+        ]);
     }
 
     /**
      * @param MemoRequest $request
      * @param $id
+     * @return array{status: bool}
      */
-    public function updateMemosById(MemoRequest $request, $id) {
-        $memo = Memo::where("id", $id)->update([
+    public function updateMemosById(MemoRequest $request, $id): array {
+        $status = Memo::where("id", $id)->update([
             "title" => $request->input("title"),
             "body" => $request->input("body"),
             "status" => $request->input("status"),
         ]);
+
+        return [
+            'status' => $status,
+        ];
+    }
+
+    /**
+     * Delete Memo by ID
+     * @param MemoRequest $request
+     * @param $id
+     */
+    public function deleteMemosById(Request $request, $id): void {
+        Memo::where("id", $id)->delete();
     }
 }
